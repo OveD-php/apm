4282ca931cc96a53ba943d97a441061bacbd1b67
vistik <hr.kloft@gmail.com>
2018-08-31 00:47:07 +0200

    The cleanup command

---
+++ src/Commands/CleanUp.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Vistik\Apm\Commands;
+
+use Carbon\Carbon;
+use DateTimeZone;
+use Illuminate\Console\Command;
+use Vistik\Apm\Query;
+use Vistik\Apm\Request;
+
+class CleanUp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'apm:cleanup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete old request log entries permanently.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $days = config('apm.keepRecordsForDays');
+        $deleteOlderThan = Carbon::parse('-' . $days . ' days', new DateTimeZone('UTC'));
+
+        Request::where('requested_at', '<', $deleteOlderThan)->delete();
+        Query::where('requested_at', '<', $deleteOlderThan)->delete();
+
+    }
+}
