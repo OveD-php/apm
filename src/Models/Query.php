16ada125667398d7c6f601790f58d9dfef7f7cdb
vistik <hr.kloft@gmail.com>
2018-08-31 00:53:04 +0200

    try to get rid of updated_at

--- src/Models/Query.php
+++ src/Models/Query.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Query extends Model
 {
-    public $timestamps = false;
-
     protected $table = 'apm_queries';
     protected $guarded = ['id'];
-    protected $dates = ['executed_at'];
 }
