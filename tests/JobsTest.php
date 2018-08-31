457b5db13a899d1d1d7a6c309ee1f4cd655f8840
vistik <hr.kloft@gmail.com>
2018-08-31 23:33:55 +0200

    added test coverage

---
+++ tests/JobsTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+use Orchestra\Testbench\TestCase;
+use Ramsey\Uuid\Uuid;
+use Vistik\Apm\Jobs\StoreQueries;
+use Vistik\Apm\Jobs\StoreRequestData;
+use Vistik\Apm\Request\ApmContext;
+use Vistik\Apm\Request\RequestResponseData;
+use Vistik\Apm\Sampling\AlwaysOn;
+
+class JobsTest extends ApmTestCase
+{
+
+    /** @test */
+    public function can_write_queries_to_log()
+    {
+        // Given
+        $this->migrate();
+        Log::shouldReceive('debug')->twice();
+
+        $this->app['config']->set('apm.saveQueriesToLog', true);
+        $context = new ApmContext(new AlwaysOn());
+        $context->addQuery('select * from users', 3.45, [], 'testing');
+        $context->addQuery('select * from users where id = ?', 1.23, [4], 'testing');
+
+        $job = new StoreQueries($context);
+
+        // When
+        $job->handle();
+
+        // Then
+        // Assertion is Log::shouldReceive('debug')->twice();
+    }
+
+    /** @test */
+    public function can_write_requests_to_log()
+    {
+        // Given
+        $this->migrate();
+        Log::shouldReceive('debug')->once();
+
+        $this->app['config']->set('apm.saveRequestsToLog', true);
+
+        $data = new RequestResponseData(
+            Uuid::uuid4()->toString(),
+            1,
+            'hello!',
+            'GET',
+            'http://localhost/path',
+            '127.0.0.1',
+            200,
+            'world?',
+            6.66,
+            ['random' => 'header'],
+            Carbon::now()
+        );
+
+        $job = new StoreRequestData($data);
+
+        // When
+        $job->handle();
+
+        // Then
+        // Assertion is Log::shouldReceive('debug')->once();
+    }
+}
